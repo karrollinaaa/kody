5367896a44bef0cdf20f6f7bfcf40f481341cdfe
karrollinaaa <karolinab503@gmail.com>
2018-10-10 07:54:27 +0200

    python

--- python/liczby_2_3.py
+++ python/liczby_2_3.py
@@ -2,18 +2,32 @@
 # -*- coding: utf-8 -*-
 
 
+def liczby2(a=10, b=99):
+    
+    """
+    Program drukuje wszystkie liczby dwucyfrowe o niepowtarzających się
+    cyfrach oraz zwraca ilość takich liczb
+    """
+    for liczba in range(10, 99):
+        if (liczba % 11) != 0:
+            print(liczba)
+        print()
+			          
+        
+    
+def liczby3(a=100, b=999):
+    
+    """
+    Program drukuje wszystkie liczby trzycyfrowe o niepowtarzających się
+    cyfrach oraz zwraca ilość takich liczb
+    """
+
 def main(args):
     
-    a = int(input("Podaj górny zakres: "))
-    
-    for i in range(1, a + 1):
-        for j in range(1, a + 1):
-                print("{:>4}" .format(i*j), end=' ')
-        print()
+    print(liczby2())
     
     return 0
-    
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
