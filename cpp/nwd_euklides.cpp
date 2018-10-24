6459ed229291ef2cec5cd91b05121d4ca14c337a
karrollinaaa <karolinab503@gmail.com>
2018-10-24 07:55:28 +0200

    minmax

--- cpp/nwd_euklides.cpp
+++ cpp/nwd_euklides.cpp
@@ -7,22 +7,18 @@
 
 #include <iostream>
 #include <iomanip>
-
 using namespace std;
-
-int euklides(int a,int b) {
+int euklides(int a, int b) {
     int licznik = 0;
     while(a > 0){
         a = a % b;
         b = b - a;
         licznik += 1;
     }
-    cout << "Powtórzeń: " << licznik << endl;
-    return b;
+    cout << "Powtorzeń: " << licznik << endl;
+    return b;    
 }
-
-int nwd_klasyczny(int a,int b) {
-    
+int nwd_klasyczny(int a, int b) {
     int licznik = 0;
     while(a != b){
         if (a > b) {
@@ -34,24 +30,19 @@ int nwd_klasyczny(int a,int b) {
             licznik += 1;
             }
         }
-    cout << "Powtórzeń: " << licznik << endl;
-    return b;
+    cout << "Powtorzeń: " << licznik << endl;
+    return b;    
 }
-
 int main(int argc, char **argv)
 {
-    int a, b;
-    a = b = 0;
-    
-    cout << "Podaj liczbę a: ";
+	int a, b; //deklaracja
+    a = b = 0; //inicjacja
+    cout << "Podaj liczbę pierwszą: ";
     cin >> a;
-    
-    cout << "Podaj liczbę b: ";
+    cout << "Podaj liczbę drugą: ";
     cin >> b;
-    
     cout << nwd_klasyczny(a, b) << endl;
     cout << euklides(a, b);
-    
 	return 0;
 }
 
---
+++ python/minmax.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  minmax.py
+#  
+#  Copyright 2018  <>
+#  
+
+import random
+
+def minmax1():
+    a = int(input('Podaj liczbę: '))
+    min = a
+    max = a
+    
+    while True:
+        a = int(input('Podaj liczbę: '))
+        if a < 1:
+            break 
+        if a < min:
+            min = a
+        if a > max:
+            max = a
+            
+    return min, max
+    
+
+def minmax2(lista):
+    min = liczba
+    max = liczba
+    
+    for liczba in (lista):
+        if liczba < min:
+            liczba = min
+        if liczba > max:
+            liczba = max
+            
+    return min, max
+        
+    
+
+def main(args):
+    #min, max = minmax1()
+    
+    #print("najmniejsza liczba: ", min)
+    #print("najwieksza liczba: ", max)
+    
+    
+    lista = []
+    for i in range(100):
+        lista.append(random.randint(1, 100))
+    print(lista)
+    min, max = minmax2(lista)
+    
+    print("najmniejsza liczba: ", min)
+    print("najwieksza liczba: ", max)
+        
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
