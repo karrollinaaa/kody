ce7aadea067d8202c4b414b322bee15e181142ef
karrollinaaa <karolinab503@gmail.com>
2018-11-06 14:09:05 +0100

    anagramy

---
+++ cpp/anagram2.cpp
@@ -0,0 +1,28 @@
+/*
+ * anagram2.cpp
+ * 
+ * Copyright 2018  <>
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	char wyraz[5]="pora";
+    int i1, i2, i3, i4;
+    for(i1 = 0; i1 < 4; i1++) {
+        for(i2 = 0; i2 < 4; i2++) {
+            if(i1==i2) continue;
+            for(i3 = 0; i3 < 4; i3++) {
+                i4=1;
+                cout << i1 << i2 << i3 << i4 << endl;
+            }
+        }
+    }
+    
+	return 0;
+}
+
