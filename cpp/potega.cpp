de6febb389f9804450e66f6a07558f7bf8368b1e
karrollinaaa <karolinab503@gmail.com>
2018-11-28 07:54:20 +0100

    defu

--- cpp/potega.cpp
+++ cpp/potega.cpp
@@ -1,25 +1,25 @@
 /*
- * potega.cpp
+ * potęga.cpp
  * 
  * Copyright 2018  <>
  */
-
-
 #include <iostream>
 using namespace std;
 
-float potega_re(float x, int n) {
+
+float potega_re(float x, int n)
+{
     if (n == 0) return 1;
     return potega_re(x, n-1) * x;
 }
 
+
 int main(int argc, char **argv)
 {
-	float podstawa;
+    float podstawa;
     int wykladnik;
-    cout << "Podstawa "; cin >> podstawa;
-    cout << "Wykładnik "; cin >> wykladnik;
-    cout << "Wynik "; cin >> potega_re(podstawa, wykladnik);
+    cout << "Podstawa: "; cin >> podstawa;
+    cout << "Wykładnik: "; cin >> wykladnik;
+    cout << "Wynik: " << potega_re(podstawa, wykladnik);
 	return 0;
 }
-
