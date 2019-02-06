a8c44ee27b1960a6cfd0af96dd3b6dd5254bb955
karrollinaaa <karolinab503@gmail.com>
2019-02-06 11:30:28 +0100

    dec2bin

--- cpp/dec2bin.cpp
+++ cpp/dec2bin.cpp
@@ -5,30 +5,64 @@
 
 
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-void dec2bin(int l) {
-    int i=0,tab[31];
+int cyfry[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 65, 66, 67, 68, 69, 70};
 
-    while(l)
-	{
-		tab[i++]=l%2;
-		l/=2;
-	}
-
-	for(int j=i-1;j>=0;j--)
-		cout<<tab[j];
+int dec2any(int liczba, int podstawa, int tab[]) {
+    int i=0;
+    do {
+        tab[i] = liczba % podstawa;
+        liczba = liczba / podstawa;
+        i++;
+    } while (liczba != 0);
+    return i - 1;
 }
 
-int main()
+void any2dec(int tab[]) {
+    int podstawa;
+    do {
+            cout << "podstawa [2,9]: ";
+            cin >> podstawa;
+    } while(podstawa < 2 || podstawa > 9);
+    
+    int ile = 0;
+    cout << "ile cyfr?";
+    cin >> ile;
+    for (int i = 0; i < ile; i++)
+        do {
+            cout << "podaj cyfre [0-" << podstawa-1 << "]: ";
+            cin >> tab[i];
+        } while (tab[i] < 0 || tab[i] > podstawa-1);
+        
+    int liczba10 = 0;
+    for (int i = 0; i < ile; i++) {
+        liczba10 = liczba10 + pow(podstawa, ile-1);
+        ile--;
+    }
+    cout << "wynik: " << liczba10;
+}
+
+
+int main(int argc, char **argv)
 {
-	int l;
-
-	cout<<"Podaj liczbę: ";
-	cin>>l;
-
-	cout<<l<<" po zamianie na postać binarną: ";
-	dec2bin(l);
-	cout<<endl;
+    int tab[8];
+    int liczba, podstawa;
+    cout << "Podaj liczbę i podstawę systemu docelowego: ";
+    cin >> liczba >> podstawa;
+    int i = dec2any(liczba, podstawa, tab);
+    cout << "Wynik: ";
+    while (i > -1) {
+        if  (podstawa > 10)
+            cout << (char)cyfry[tab[i]];
+        else
+            cout << tab[i];
+        i--;
+    }
+    cout << endl;
+    any2dec(tab);
 	return 0;
 }
+
