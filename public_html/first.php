d6696ebe80cb0e407bfbdc1296271cacffa4d0db
karrollinaaa <karolinab503@gmail.com>
2019-02-22 07:55:03 +0100

    php

---
+++ public_html/first.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<!--
+   first.php
+   
+   Copyright 2019  <>
+-->
+
+<head>
+<meta charset="utf-8" />
+<title>TEST PHP</title>
+<meta name="generator" content="Geany 1.32" />
+<?php
+
+?>
+</head>
+
+<body>
+<?php
+    echo("<h1>Witaj w php!</h1>");
+?>
+</body>
+
+</html>
